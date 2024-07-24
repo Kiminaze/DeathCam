7332bc96e6fe97ac1e1614843a79d8a99c515e91
Kiminaze <kiminaze@yahoo.de>
2024-07-25 01:51:14 +0200

    Switched to fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+
+fx_version "cerulean"
+games { "gta5" }
+
+author "Philipp Decker"
+description "Rotate the camera around the player when you are dead."
+version "1.1.0"
+
+client_scripts {
+	"config.lua",
+	"client.lua"
+}
